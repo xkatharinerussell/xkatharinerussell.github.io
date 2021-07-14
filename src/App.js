56432053269c2138fee10097576296f506362b9e
Katharine Russell <katharinezrussell@gmail.com>
2021-07-14 12:39:32 +1000

    Add nav and basic styling to home page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { Box, Menu, VStack, HStack, Flex, Heading, IconButton, useColorMode, Spacer } from '@chakra-ui/react';
+import { FaSun, FaMoon, FaEnvelope, FaGithub, FaLinkedinIn } from 'react-icons/fa';
+
+import Nav from './Nav';
 
 function App() {
+
+  const { colorMode, toggleColorMode } = useColorMode();
+  const isDark = colorMode === "dark";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <VStack>
+      <Nav></Nav>
+      
+      <Box p={40}>
+        <Heading color="orange.400" fontSize="7em">Hi I'm Katharine, a software engineer in Sydney.</Heading>
+      </Box>
+
+      <HStack alignItems={'center'} spacing={10}>
+        <IconButton icon={<FaEnvelope />} isRound="true" size="lg"></IconButton>
+        <IconButton icon={<FaGithub />} isRound="true" size="lg"></IconButton>
+        <IconButton icon={<FaLinkedinIn />} isRound="true" size="lg"></IconButton>
+      </HStack>
+
+    </VStack>
   );
 }
 
